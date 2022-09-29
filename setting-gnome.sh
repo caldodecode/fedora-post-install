3fcd0523e53d33d438f984bdbdf6b18214da279c
Varela <Varela>
2022-09-29 01:34:00 -0300

    ...

--- setting-gnome.sh
+++ setting-gnome.sh
@@ -1,3 +1,17 @@
+### INSTALL GNOME ###
+sudo dnf install -y \
+ gdm \
+ google-noto-fonts-common \
+ gnome-themes-extra \
+ gnome-shell \
+ gnome-terminal \
+ gnome-tweaks \
+ nautilus \
+ dbus-x11 
+
+### SET DEFAULT TARGET AS GRAPHICAL ###
+sudo systemctl set-default graphical.target 
+
 ### SET DARK THEME GTK4+ ###
 gsettings set org.gnome.desktop.interface color-scheme 'prefer-dark'
 
@@ -9,7 +23,7 @@ gsettings set org.gnome.desktop.wm.keybindings switch-applications "[]"
 gsettings set org.gnome.desktop.wm.keybindings switch-windows "['<Alt>Tab']"
 
 ### DOWNLOAD BACKGROUND ###
-curl -L --output wallpaper.jpg https://lh3.googleusercontent.com/drive-viewer/AJc5JmS6t5f1Jb8Jd5Bpi2C7F6lb4GPMMJ8QCLBnddA5prHm3nMuSsMq5OGDzsgbEPkydEgzaXrJ378=w1920-h1005
+sudo curl -L --output wallpaper.jpg https://lh3.googleusercontent.com/drive-viewer/AJc5JmS6t5f1Jb8Jd5Bpi2C7F6lb4GPMMJ8QCLBnddA5prHm3nMuSsMq5OGDzsgbEPkydEgzaXrJ378=w1920-h1005
 
 ### SET BACKGROUND ###
 gsettings set org.gnome.desktop.background picture-uri-dark  file://$(pwd)/wallpaper.jpg
